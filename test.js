7db3e66fb6a684c4aef8b47ccf9997d0b82931c6
Simone Busoli <simone.busoli@gmail.com>
2020-11-28 10:22:07 +0100

    chore: switch to node-fetch

--- test.js
+++ test.js
@@ -585,7 +585,7 @@ describe('RS256 JWT token validation', function () {
       code: 'ECONNREFUSED',
       statusCode: 500,
       error: 'Internal Server Error',
-      message: 'connect ECONNREFUSED 127.0.0.1:443'
+      message: 'request to https://localhost/.well-known/jwks.json failed, reason: connect ECONNREFUSED 127.0.0.1:443'
     })
   })
 
@@ -639,7 +639,7 @@ describe('RS256 JWT token validation', function () {
       error: 'Internal Server Error'
     })
 
-    expect(body.message).toMatch(/^Nock: No match for request/)
+    expect(body.message).toMatch(/Nock: No match for request/)
   })
 
   it('should not cache the key if cache was disabled', async function () {
@@ -670,7 +670,7 @@ describe('RS256 JWT token validation', function () {
       error: 'Internal Server Error'
     })
 
-    expect(body.message).toMatch(/^Nock: No match for request/)
+    expect(body.message).toMatch(/Nock: No match for request/)
   })
 
   it('should not try to get the key twice when using caching if a previous attempt failed', async function () {
