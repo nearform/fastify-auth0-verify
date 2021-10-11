3d92ac36541427ea0ab191ddf272764645c98c88
brainrepo <murru7@gmail.com>
2021-10-11 17:37:10 +0200

    fix(types): fix decode types

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,7 +1,8 @@
 import Fastify from 'fastify'
 import fastifyAuth0Verify from '.'
 import { expectAssignable, expectType } from 'tsd'
-import { DecodeOptions, JwtPayload, Jwt } from 'jsonwebtoken'
+import { DecodePayloadType, FastifyJwtDecodeOptions } from 'fastify-jwt'
+import fastifyJWT from 'fastify-jwt'
 
 const fastify = Fastify()
 
@@ -13,21 +14,31 @@ fastify.register(fastifyAuth0Verify, {
   domain: '<auth0 auth domain>',
   audience: ['<auth0 app audience>', '<auth0 admin audience>']
 })
+fastify.register(fastifyAuth0Verify, {
+  domain: '<auth0 auth domain>',
+  audience: ['<auth0 app audience>', '<auth0 admin audience>']
+})
+fastify.register(fastifyJWT, {
+  secret: '<jwt secret>',
+})
 
 fastify.register(function (instance, _options, done) {
   instance.get('/verify', {
     handler: function (request, reply) {
-      expectAssignable<Function>(request.jwtDecode)
+      expectAssignable<Function>(request.jwtDecode);
 
-      const options: DecodeOptions = {
-        complete: true,
-        json: true
+      const options: FastifyJwtDecodeOptions = {
+        decode:{
+          complete: true,
+          json: true
+        },
+        verify:{}
       }
 
-      expectType<null | JwtPayload | Jwt | string>(request.jwtDecode(options))
-      expectType<null | JwtPayload | Jwt | string>(request.jwtDecode({ json: true }))
-      expectType<null | JwtPayload | Jwt | string>(request.jwtDecode({ complete: true }))
-      expectType<null | JwtPayload | Jwt | string>(request.jwtDecode())
+      expectType<Promise<DecodePayloadType>>(request.jwtDecode(options))
+      expectType<Promise<DecodePayloadType>>(request.jwtDecode({decode:{ json: true }, verify:{}}))
+      expectType<Promise<DecodePayloadType>>(request.jwtDecode({decode:{ complete: true }, verify:{}}))
+      expectType<Promise<DecodePayloadType>>(request.jwtDecode())
 
       reply.send(request.user)
     },
