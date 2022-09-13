8c1e1acfdd833e4fcda23217001f63ae94b1ba3f
erik sundeqvist <erik.sundeqvist@acast.com>
2022-09-13 18:07:21 +0200

    fix: allow array of string in issuer type

--- index.test-d.ts
+++ index.test-d.ts
@@ -8,10 +8,12 @@ const fastify = Fastify()
 
 fastify.register(fastifyAuth0Verify, {
   domain: '<auth0 auth domain>',
+  issuer: '<auth0 issuer>',
   audience: '<auth0 app audience>'
 })
 fastify.register(fastifyAuth0Verify, {
   domain: '<auth0 auth domain>',
+  issuer: ['<auth0 issuer>'],
   audience: ['<auth0 app audience>', '<auth0 admin audience>']
 })
 fastify.register(fastifyAuth0Verify, {
