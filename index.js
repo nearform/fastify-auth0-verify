3a7cf3d397eaaf6adb7df08cd6507e4375ee0f42
Fergus Ruston <fergus.ruston@nearform.com>
2023-05-12 14:10:26 +0100

    feat: improved options verification before passing options to fastify-jwt-jwks

--- index.js
+++ index.js
@@ -3,12 +3,25 @@
 const fastifyPlugin = require('fastify-plugin')
 const fastifyJwtJwks = require('fastify-jwt-jwks').fastifyJwtJwks
 
+const errorMessages = {
+  missingOptions: 'Please provide at least one of the "domain" or "secret" options.'
+}
+
 function fastifyAuth0Verify(instance, options, done) {
-  if (options.domain) {
-    const domain = options.domain
+  const { domain, secret } = options
+
+  if (!domain && !secret) {
+    // Domain or secret are required for verification
+    // Checking for secret here prevents a misleading error message if neither
+    // jwksUrl or secret are passed to fastify-jwt-jwks
+    throw new Error(errorMessages.missingOptions)
+  }
+
+  if (domain) {
     delete options.domain
     options.jwksUrl = `${domain}.well-known/jwks.json`
   }
+
   return fastifyJwtJwks(instance, options, done)
 }
 
