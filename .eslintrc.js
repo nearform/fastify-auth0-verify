4048977056c8bddef94c70accca01a6713611ae5
Fergus Ruston <fergus.ruston@nearform.com>
2023-05-15 11:56:07 +0100

    Revert "chore: update eslint config to be consistent with recent packages"
    This reverts commit 7d8dd19dd71b97ac99a1c56af7b8d76251f39fed.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,5 @@
 module.exports = {
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
-  env: {
-    "node": true,
-    "es2021": true
-  },
-  parserOptions: {
-    "ecmaVersion": 2022,
-    "sourceType": "module"
-  },
   overrides: [
     {
       extends: ['eslint:recommended', 'plugin:prettier/recommended'],
