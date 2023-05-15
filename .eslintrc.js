7d8dd19dd71b97ac99a1c56af7b8d76251f39fed
Fergus Ruston <fergus.ruston@nearform.com>
2023-05-15 11:04:41 +0100

    chore: update eslint config to be consistent with recent packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,13 @@
 module.exports = {
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  env: {
+    "node": true,
+    "es2021": true
+  },
+  parserOptions: {
+    "ecmaVersion": 2022,
+    "sourceType": "module"
+  },
   overrides: [
     {
       extends: ['eslint:recommended', 'plugin:prettier/recommended'],
